8a0419ef701c80ab9a0944d43498b6c1cc60b083
Kevin J Walters <kjw.github@gmail.com>
2019-04-08 01:28:42 +0100

    Adapting to interface changes in latest incarnation of the adafruit_midi library including triming everything to try and git in memory but this is currently very fragile and blows up eventually on wavetable changes.

--- cpx/cpx-basic-standalone-synth.py
+++ cpx/cpx-basic-standalone-synth.py
@@ -1,9 +1,9 @@
-### cpx-basic-square-monosynth v1.3
+### cpx-basic-square-monosynth v1.4
 ### CircuitPython (on CPX) synth module using internal speaker
 ### Monophonic synth with some velocity sensitivity and a few
 ### different waveforms
 
-### Tested with CPX and CircuitPython and 4.0.0-beta.2-141-g2c9fbb5d4-dirty
+### Tested with CPX and CircuitPython and 4.0.0-beta.5
 
 ### Needs recent adafruit_midi module
 
@@ -39,12 +39,21 @@ import math
 import digitalio
 import audioio
 import board
-
 import neopixel
- 
 
+import usb_midi
 
 import adafruit_midi
+
+from adafruit_midi.midi_message     import note_parser
+
+from adafruit_midi.note_on          import NoteOn
+from adafruit_midi.note_off         import NoteOff
+from adafruit_midi.control_change   import ControlChange
+from adafruit_midi.pitch_bend       import PitchBend
+from adafruit_midi.program_change   import ProgramChange
+
+### TODO - deal with max sample playback rate of 350000
 
 ### TODO - add control over this
 ### and work out what's going on at 97-98
@@ -56,35 +65,44 @@ dac = audioio.AudioOut(board.SPEAKER)
 
 ### TO RESEARCH - is const more memory efficient?
 A4refhz = const(440)
-midinoteA4 = const(69)
-midinoteC4 = const(60)
-basesamplerate = 42240  ### this makes A4 exactly 96 samples
+midinoteC4 = note_parser("C4")
+midinoteA4 = note_parser("A4")
+
+### TODO - review this - way too fragile with MemoryError problems
+basesamplerate = 42240  ### this makes A4 exactly 96 samples (440*96)
 
 wavename = "square"
 wavenames = ["square", 
              "sawtooth", "supersaw", "supersupersaw", 
-             "sine", "sineoct2", "sinefifth", "sinemajorchord"]
-
+             "sine", "sineoct2", "sinefifth"]  ## removed "sinemajorchord" for now
 
 ### brightness 1.0 saves memory by removing need for a second buffer
-### 10 is number of NeoPixels on 
+### 10 is number of NeoPixels on
 numpixels = const(10)
+### brightness of 1.0 prevents an extra array from being created
 pixels = neopixel.NeoPixel(board.NEOPIXEL, numpixels, brightness=1.0)
-black = (0, 0, 0)
-pixels.fill(black)
 
-### Turn NeoPixel on to represent a note using RGB x 10 
+### Turn NeoPixel on to represent a note using RGB x 10
 ### to represent 30 notes
 ### Doesn't do anything with pitch bend
 def noteled(pixels, note, velocity):
     note30 = ( note - midinoteC4 ) % (3 * numpixels)
     pos = note30 % numpixels
+    r, g, b = pixels[pos]
     if velocity == 0:
-        colour = black
+        brightness = 0
     else:
-        ### max brightness will be 32    
-        colour = round(velocity / 127 * 30 + 2) << ((2 - note30 // numpixels) * 8)
-    pixels[pos] = colour
+        ### max brightness will be 32
+        brightness = round(velocity / 127 * 30 + 2)
+    ### Pick R/G/B based on range within the 30 notes
+    if note30 < 10:
+        r = brightness
+    elif note30 < 20:
+        g = brightness
+    else: 
+        b = brightness
+    pixels[pos] = (r, g, b)
+
 
 ### white pulsing for whatever patch we just selected
 flashbrightness = 20
@@ -98,7 +116,7 @@ def flashpatch(pixels, patchnum):
         for i in range(flashbrightness, 0, -2):
             pixels[pos] = (i, i, i)
     pixels[pos] = oldcolour
-        
+
 
 ### TODO - improve commenting
 
@@ -109,6 +127,7 @@ def flashpatch(pixels, patchnum):
 ### TODO - consider putting in some 32768 values on square and sawtooths
 ###        to make them finish on midpoint and remove any under
 ###        the covers slewing 
+### TODO - check frequencies are correct on the cyclelength ones
 def makewaves(waves, type, samplerate):
     cyclelength = round(samplerate // A4refhz)
     length = cyclelength  ### a default which some waves will change
@@ -123,93 +142,93 @@ def makewaves(waves, type, samplerate):
     ### major chord has longer sample which initially works
     ### but will blow up later with four levels
     if type == "majorchord":
-        volumes = [23000]
+        volumes = [23000]   ### 32000 would go out of bounds
     else:
-        volumes = [10000, 18000, 23000, 30000]
+        volumes = [30000]  # running too tight on memory for multiple values at 96 samples TODO review this
 
     ### Make some waves at different volumes
     for vol in volumes:
         ### Need to create a new array here as audio.RawSample appears not
         if type == "square":
             waveraw = array.array("h", [-vol] * (length // 2) + [vol] * (length - length // 2))
-        elif type == "sawtooth":
-            waveraw = array.array("h", [0] * length)
-            for i in range(length):
-                waveraw[i] = round((i / (length - 1) - 0.5) * 2 * vol)
-        elif type == "supersaw":
-            waveraw = array.array("h", [0] * length)
-            halflength    = length // 2
-            quarterlength = length // 4
-            for i in range(length):
-                ### TODO replace this with a gen function similar to sine
-                waveraw[i] = round(((i / (length - 1) - 0.5 +
-                                     i % halflength / (halflength - 1) - 0.5 +
-                                     i % quarterlength / (quarterlength - 1) - 0.5)
-                                   ) * 2 / 3 * vol)
-        elif type == "supersupersaw":
-            cycles = 2
-            length = cycles * cyclelength
-            waveraw = array.array("h", [0] * length)
-            ### TODO add two 5ths
-            twothirdsclen = 2 * cyclelength // 3
-            halfclen    = cyclelength // 2
-            thirdclen   = cyclelength // 3
-            quarterclen = cyclelength // 4
-            sixthclen   = cyclelength // 6
-            eighthlen   = cyclelength // 8
-            for i in range(length):
-                ### TODO replace this with a gen function similar to sine
-                waveraw[i] = round((
-                                    2/8 * (i                 / (cyclelength - 1) - 0.5) +
-                                    1/8 * (i % twothirdsclen / (twothirdsclen - 1) - 0.5) +
-                                    1/8 * (i % halfclen      / (halfclen - 1) - 0.5) +
-                                    1/8 * (i % thirdclen     / (thirdclen - 1) - 0.5) +
-                                    1/8 * (i % quarterclen   / (quarterclen - 1) - 0.5) + 
-                                    1/8 * (i % sixthclen     / (sixthclen - 1) - 0.5) +
-                                    1/8 * (i % eighthlen     / (eighthlen - 1) - 0.5)
-                                   ) * 2 * vol)
-        elif type == "sine":
-            waveraw = array.array("h", [0] * length)
-            for i in range(length):
-                waveraw[i] = round(math.sin(math.pi * 2 * i / length) * vol)
-        elif type == "sineoct2":
-            waveraw = array.array("h", [0] * length)
-            for i in range(length):
-                waveraw[i] = round((math.sin(math.pi * 2 * i / length) * 2/3 +
-                                    math.sin(math.pi * 2 * i / length * 2) * 1/3
-                                   ) * vol)
-        elif type == "sinefifth":
-            cycles = 2
-            length = cycles * cyclelength
-            waveraw = array.array("h", [0] * length)
-            for i in range(length):
-                waveraw[i] = round((math.sin(math.pi * 2 * i / cyclelength) +
-                                    math.sin(math.pi * 2 * i / cyclelength * 3/2)
-                                   ) * vol / 2)
-        elif type == "sinemajorchord":
-            cycles = 4
-            length = cycles * cyclelength
-            waveraw = array.array("h", [0] * length)
-            for i in range(length):
-                waveraw[i] = round((math.sin(math.pi * 2 * i / cyclelength) +
-                                    math.sin(math.pi * 2 * i / cyclelength * 5/4) +
-                                    math.sin(math.pi * 2 * i / cyclelength * 6/4)
-                                   ) * vol / 2.5)
         else:
-            return ValueError("Unknown type")
+            if type == "sawtooth":
+                waveraw = array.array("h", [0] * length)
+                for i in range(length):
+                    waveraw[i] = round((i / (length - 1) - 0.5) * 2 * vol)
+            elif type == "supersaw":
+                waveraw = array.array("h", [0] * length)
+                halflength    = length // 2
+                quarterlength = length // 4
+                for i in range(length):
+                    ### TODO replace this with a gen function similar to sine
+                    waveraw[i] = round(((i / (length - 1) - 0.5 +
+                                         i % halflength / (halflength - 1) - 0.5 +
+                                         i % quarterlength / (quarterlength - 1) - 0.5)
+                                       ) * 2 / 3 * vol)
+            elif type == "supersupersaw":
+                cycles = 2
+                length = cycles * cyclelength
+                waveraw = array.array("h", [0] * length)
+                ### TODO add two 5ths
+                twothirdsclen = 2 * cyclelength // 3
+                halfclen    = cyclelength // 2
+                thirdclen   = cyclelength // 3
+                quarterclen = cyclelength // 4
+                sixthclen   = cyclelength // 6
+                eighthlen   = cyclelength // 8
+                for i in range(length):
+                    ### TODO replace this with a gen function similar to sine
+                    waveraw[i] = round((
+                                        2/8 * (i                 / (cyclelength - 1) - 0.5) +
+                                        1/8 * (i % twothirdsclen / (twothirdsclen - 1) - 0.5) +
+                                        1/8 * (i % halfclen      / (halfclen - 1) - 0.5) +
+                                        1/8 * (i % thirdclen     / (thirdclen - 1) - 0.5) +
+                                        1/8 * (i % quarterclen   / (quarterclen - 1) - 0.5) + 
+                                        1/8 * (i % sixthclen     / (sixthclen - 1) - 0.5) +
+                                        1/8 * (i % eighthlen     / (eighthlen - 1) - 0.5)
+                                       ) * 2 * vol)
+            elif type == "sine":
+                waveraw = array.array("h", [0] * length)
+                for i in range(length):
+                    waveraw[i] = round(math.sin(math.pi * 2 * i / length) * vol)
+            elif type == "sineoct2":
+                waveraw = array.array("h", [0] * length)
+                for i in range(length):
+                    waveraw[i] = round((math.sin(math.pi * 2 * i / length) * 2/3 +
+                                        math.sin(math.pi * 2 * i / length * 2) * 1/3
+                                       ) * vol)
+            elif type == "sinefifth":
+                cycles = 2
+                length = cycles * cyclelength
+                waveraw = array.array("h", [0] * length)
+                for i in range(length):
+                    waveraw[i] = round((math.sin(math.pi * 2 * i / cyclelength) +
+                                        math.sin(math.pi * 2 * i / cyclelength * 3/2)
+                                       ) * vol / 2)
+            # elif type == "sinemajorchord":
+                # cycles = 4
+                # length = cycles * cyclelength
+                # waveraw = array.array("h", [0] * length)
+                # for i in range(length):
+                    # waveraw[i] = round((math.sin(math.pi * 2 * i / cyclelength) +
+                                        # math.sin(math.pi * 2 * i / cyclelength * 5/4) +
+                                        # math.sin(math.pi * 2 * i / cyclelength * 6/4)
+                                       # ) * vol / 2.5)
+            else:
+                raise ValueError("Unknown type")
 
         waves.append(audioio.RawSample(waveraw))
 
-
 ### 0 is MIDI channel 1
-midi = adafruit_midi.MIDI(in_channel=0)
+midi = adafruit_midi.MIDI(midi_in=usb_midi.ports[0], in_channel=0)
 
 #veltovol = int(65535 / 127)
 ### Multiplier for MIDI velocity ^ 0.40
 ### 0.5 would be correct for velocity = power
 ### but 0.4 sounds more natural - ymmv
-velcurve = 0.40
-veltovolc040 = 9439
+#velcurve = 0.40
+#veltovolc040 = 9439
 
 # pitchbendrange in semitones - often 2 or 12
 pitchbendmultiplier = 12 / 8192
@@ -217,19 +236,17 @@ pitchbendvalue = 8192  # mid point - no bend
 
 debug = False
 
-
 lastnote = None
 modwheel = 0
 
 waves = []
 makewaves(waves, wavename, basesamplerate)
 
-
 while True:
-    (msg, channel) = midi.read_in_port()
-    if isinstance(msg, adafruit_midi.NoteOn) and msg.velocity != 0:
+    msg = midi.receive()
+    if isinstance(msg, NoteOn) and msg.velocity != 0:
 #        if debug:
-#            print("NoteOn", msg.note, msg.velocity)
+#            print("NoteOn", msg.note, msg.velocity, msg.channel)
         lastnote = msg.note
         pitchbend = (pitchbendvalue - 8192) * pitchbendmultiplier
         notefreq = round(A4refhz * math.pow(2, (lastnote - midinoteA4 + pitchbend) / 12.0))
@@ -247,10 +264,10 @@ while True:
 
         noteled(pixels, msg.note, msg.velocity)
 
-    elif (isinstance(msg, adafruit_midi.NoteOff) or 
-          isinstance(msg, adafruit_midi.NoteOn) and msg.velocity == 0):
+    elif (isinstance(msg, NoteOff) or 
+          isinstance(msg, NoteOn) and msg.velocity == 0):
 #        if debug:
-#            print("NoteOff", msg.note, msg.velocity)
+#            print("NoteOff", msg.note, msg.velocity, msg.channel)
         # Our monophonic "synth module" needs to ignore keys that lifted on
         # overlapping presses
         if msg.note == lastnote:
@@ -261,7 +278,7 @@ while True:
 #    elif msg is not None:
 #        if debug:
 #            print("Something else:", msg)
-    elif isinstance(msg, adafruit_midi.PitchBendChange):
+    elif isinstance(msg, PitchBend):
         pitchbendvalue = msg.pitch_bend   ### 0 to 16383
         ### TODO - undo cut and paste here
         pitchbend = (pitchbendvalue - 8192) * pitchbendmultiplier
@@ -276,10 +293,10 @@ while True:
         wave.sample_rate = round(notesamplerate)  ### integer only
         dac.play(wave, loop=True)
         
-    elif isinstance(msg, adafruit_midi.ProgramChange):
-        print("patch select", msg.patch)
-        
-    elif isinstance(msg, adafruit_midi.ControlChange):
+#    elif isinstance(msg, ProgramChange):
+#        print("patch select", msg.patch)
+#        
+    elif isinstance(msg, ControlChange):
         if msg.control == 1:  # modulation wheel - TODO MOVE THIS TO adafruit_midi
             ### msg.value is 0 (none) to 127 (max)
             modwheel = msg.value
