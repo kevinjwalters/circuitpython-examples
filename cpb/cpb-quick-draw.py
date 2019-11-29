d3cbab66e4b6907d055bc6546aa0d3fcba4778c8
Kevin J Walters <kjw.github@gmail.com>
2019-11-29 23:38:24 +0000

    Renaming PingPacket to TimePacket and using it to exchange the response time which is now measured. Display similar to the original Quick Draw Learn Guide.

--- cpb/cpb-quick-draw.py
+++ cpb/cpb-quick-draw.py
@@ -1,4 +1,4 @@
-### cpb-quick-draw v0.9
+### cpb-quick-draw v1.0
 ### CircuitPython (on CPB) Quick Draw reaction game
 
 ### Tested with Circuit Playground Bluefruit Alpha
@@ -51,8 +51,8 @@ import neopixel
 
 from adafruit_bluefruit_connect.packet import Packet
 
-class PingPacket(Packet):
-    """A ping packet of time.monotonic() and lastrtt."""
+class TimePacket(Packet):
+    """A time packet of time.monotonic() and lastrtt."""
 
     _FMT_PARSE = '<xxffx'
     PACKET_LENGTH = struct.calcsize(_FMT_PARSE)
@@ -64,30 +64,30 @@ class PingPacket(Packet):
 
     # number of args must match _FMT_PARSE
     # for Packet.parse_private() to work
-    def __init__(self, lastrtt, sendtime):
-        """Construct a PingPacket."""
-        self._lastrtt = lastrtt
-        self._sendtime = sendtime   # over-written later
+    def __init__(self, duration, sendtime):
+        """Construct a TimePacket."""
+        self._duration = duration
+        self._sendtime = sendtime  # over-written later
 
     def to_bytes(self):
         """Return the bytes needed to send this packet.
         """
         self._sendtime = time.monotonic()
         partial_packet = struct.pack(self._FMT_CONSTRUCT, self._TYPE_HEADER,
-                                     self._lastrtt, self._sendtime)
+                                     self._duration, self._sendtime)
         return self.add_checksum(partial_packet)
 
     @property
-    def lastrtt(self):
+    def duration(self):
         """The last rtt value or a negative number if n/a."""
-        return self._lastrtt
+        return self._duration
 
     @property
     def sendtime(self):
         """The time packet was sent (when to_bytes() was last called)."""
         return self._sendtime
 
-PingPacket.register_packet_type()
+TimePacket.register_packet_type()
 
 
 class StartGame(Packet):
@@ -132,16 +132,30 @@ else:
 # brightness 1 saves memory by removing need for a second buffer
 # 10 is number of NeoPixels on CPX
 numpixels = const(10)
+halfnumpixels = const(5)
 pixels = neopixel.NeoPixel(board.NEOPIXEL, numpixels, brightness=1)
 
 # The touch pads calibrate themselves as they are created, just once here
 touchpad = touchio.TouchIn(board.A1)
 
-# button A is on left (usb at top
-button_left = digitalio.DigitalInOut(board.BUTTON_A)
-button_left.switch_to_input(pull=digitalio.Pull.DOWN)
-button_right = digitalio.DigitalInOut(board.BUTTON_B)
-button_right.switch_to_input(pull=digitalio.Pull.DOWN)
+if master_device:
+    # button A is on left (usb at top
+    player_button = digitalio.DigitalInOut(board.BUTTON_A)
+    player_button.switch_to_input(pull=digitalio.Pull.DOWN)
+    
+    player_px = (0, halfnumpixels)
+    opponent_px = (halfnumpixels, numpixels)
+else:
+    # button B is on right
+    player_button = digitalio.DigitalInOut(board.BUTTON_B)
+    player_button.switch_to_input(pull=digitalio.Pull.DOWN)
+    
+    player_px = (halfnumpixels, numpixels)
+    opponent_px = (0, halfnumpixels)
+
+win_colour     = [(0,  30, 0) ] * halfnumpixels
+misdraw_colour = [(40, 0,  0) ] * halfnumpixels
+draw_colour    = [(40, 30, 0) ] * halfnumpixels
 
 ### From first tests master shows
 ###
@@ -150,7 +164,7 @@ button_right.switch_to_input(pull=digitalio.Pull.DOWN)
 ### RTT plus a bit 0.0429688
 ### lots repeated, occasional 0.0390625, 0.046875
 
-### With new PingPacket this is
+### With new TimePacket this is
 ### 0.0546875, 0.046875, 0.0390625, 0.046875, 0.0390625
 
 ### TODO - do some order of start-up testing and compare with previous code
@@ -169,7 +183,7 @@ offsets = []
 if master_device:
     # Master code
     scanner = Scanner()
-    uart_client = UARTClient(timeout=1.0)
+    uart_client = UARTClient(timeout=11.0)
     while True:
         uart_addresses = []
         while not uart_addresses:
@@ -180,14 +194,14 @@ if master_device:
 
         gc.collect()  # opportune moment
         while uart_client.connected:
-            request = PingPacket(rtt, 0.0)
+            request = TimePacket(rtt, 0.0)
             
             try:
                 uart_client.write(request.to_bytes())
                 # TODO - can I split from_stream into read then parse?
                 response = Packet.from_stream(uart_client)
                 t2 = time.monotonic()
-                if isinstance(response, PingPacket):
+                if isinstance(response, TimePacket):
                     print("RX")
                     rtt = t2 - request.sendtime
                     rtts.append(rtt)
@@ -212,7 +226,7 @@ if master_device:
 
 else:
     # Slave code
-    uart_server = UARTServer(timeout=1.0)
+    uart_server = UARTServer(timeout=11.0)
     responses = 0
     while True:
         uart_server.start_advertising()
@@ -223,12 +237,12 @@ else:
         while uart_server.connected:
             # TODO - consider using uart_server.in_waiting
             packet = Packet.from_stream(uart_server)
-            if isinstance(packet, PingPacket):
+            if isinstance(packet, TimePacket):
                 print("RX")
                 try:
-                    uart_server.write(PingPacket(-2.0, -1.0).to_bytes())
+                    uart_server.write(TimePacket(-2.0, -1.0).to_bytes())
                     responses += 1
-                    rtts.append(packet.lastrtt)
+                    rtts.append(packet.duration)
                     pixels.fill((0,0,10))
                     # this must be less than the client inter-packet pause
                     time.sleep(0.1)
@@ -265,8 +279,12 @@ pixels.fill((0,0,100))
 time.sleep(2)
 pixels.fill((0,0,0))
 
-
+### TODO randomise this sleep
 time.sleep(5)
+
+# TODO - ponder whether I want any gc in here or not
+# perhaps leave it for second program
+# CPB has loads (?) of memory too...
 gc.collect()
 
 if master_device:
@@ -303,8 +321,97 @@ else:
 
 # The CPBs should now be synchronised and this could will run at the
 # same time
-for idx in range(10):
-    pixels.fill((30, 30, 30))
-    time.sleep(1)
-    pixels.fill((0,0,0))
-    time.sleep(1)
+##for idx in range(10):
+##    pixels.fill((30, 30, 30))
+##    time.sleep(1)
+##    pixels.fill((0,0,0))
+##    time.sleep(1)
+
+# FOR THIS VERSION USE BUTTON AND USE IT ON THE SIDE THE SWITCH IS SET TO
+# replicate the style of the original quick draw
+# pass results between the two 
+# wait 5 seconds for data packet exchange
+# in notes document it behaves slightly differently as it cannot tell
+# how long other play took until it receives data
+# 
+
+# sound sample?
+
+# Start the game
+pixels.fill((30, 30, 30))
+start_t = time.monotonic()
+while not player_button.value:
+   pass
+finish_t = time.monotonic()
+pixels.fill((0, 0, 0))
+
+### TODO play sound
+
+player_reaction_dur = finish_t - start_t
+
+### TODO exchange times over bluetooth
+### test with length delay from other player for
+### both cases to check buffering saves the day
+error_dur = -1.0
+opponent_reaction_dur = error_dur
+if master_device:
+    if uart_client.connected:
+        try:
+            uart_client.write(TimePacket(player_reaction_dur,
+                                         0.0).to_bytes())
+            print("TimePacket TX")
+            packet = Packet.from_stream(uart_client)
+            if isinstance(packet, TimePacket):
+                print("TimePacket RX")
+                opponent_reaction_dur = packet.duration
+            elif packet is None:
+                pass
+            else:
+                print("Unexpected packet type", packet)
+        except OSError as err:
+            print(err, file=sys.stderr)
+
+else:
+    if uart_server.connected:
+        packet = Packet.from_stream(uart_server)
+        if isinstance(packet, TimePacket):
+            print("TimePacket RX")
+            opponent_reaction_dur = packet.duration
+            try:
+                uart_server.write(TimePacket(player_reaction_dur,
+                                             0.0).to_bytes())
+                print("TimePacket TX")
+            except OSError as err:
+                print(err, file=sys.stderr)
+        elif packet is None:
+            pass
+        else:
+            print("Unexpected packet type", packet)
+
+print("({:f}, {:f})".format(player_reaction_dur, opponent_reaction_dur))
+
+if player_reaction_dur < 0.1 or opponent_reaction_dur < 0.1:
+    if player_reaction_dur != error_dur and player_reaction_dur < 0.1:
+        pixels[player_px[0]:player_px[1]] = misdraw_colour
+    if opponent_reaction_dur != error_dur and opponent_reaction_dur < 0.1:
+        pixels[opponent_px[0]:opponent_px[1]] = misdraw_colour
+else:
+    if player_reaction_dur < opponent_reaction_dur:
+        pixels[player_px[0]:player_px[1]] = win_colour
+    elif opponent_reaction_dur < player_reaction_dur:
+        pixels[opponent_px[0]:opponent_px[1]] = win_colour
+    else:
+        # Very unlikely to reach here
+        pixels[player_px[0]:player_px[1]] = draw_colour
+        pixels[opponent_px[0]:opponent_px[1]] = draw_colour
+        
+### TODO         
+#print values in mu friendly format
+#print running stats in my friendly format
+
+
+### TODO - this appears to fix a bug which may related to closing the
+###        connection by program termination and other end not receiving it
+### Bluetooth connection needs to be kept connected for the read to work
+### TODO - reproduce this in smaller code
+time.sleep(5)
