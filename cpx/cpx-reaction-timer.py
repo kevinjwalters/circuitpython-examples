b590ddf935f10983ca7e99068ce8ad328d00db06
Kevin J Walters <kjw.github@gmail.com>
2020-01-04 01:30:17 +0000

    Converting to work on both CPB and CPX with conditional imports based on try/except technique. Setting the PRNG seed for any boards without os.urandom, i.e. CPX. Put some of the repeated code from the main loop into catchy-name wait_finger_off_and_random_delay(). Using SHORTEST_DELAY and LONGEST_DELAY from Quick Draw but without the integer limitation. Removed clicks - cannot remember why I was playing with that. Removed unused button code as touch pad are being used for input for this program.

--- cpx/cpx-reaction-timer.py
+++ cpx/cpx-reaction-timer.py
@@ -1,8 +1,10 @@
-### cpx-reaction-timer v0.5
+### cpx-reaction-timer v0.6
 ### A human reaction timer using light and sound with touch pads
 ### Measures the time it takes for user to press A1
 
-### Tested with CPX and CircuitPython XXX
+### Tested on
+### CPX running CircuitPython 4.1.0
+### CPB running CircuitPython 5.0.0-beta.2
 
 ### copy this file to CPX as code.py
 
@@ -38,10 +40,43 @@ import board
 import pulseio
 import touchio
 import digitalio
-import audioio
+import analogio
+import os
+
+# This code works on both CPB and CPX boards by bringing
+# in classes with same name
+try:
+    from audiocore import RawSample
+except ImportError:
+    from audioio import RawSample
+try:
+    from audioio import AudioOut
+except ImportError:
+    from audiopwmio import PWMAudioOut as AudioOut
 
 import neopixel
 import adafruit_motor.servo
+
+
+def seed_with_noise():
+    """Set random seed based on four reads from analogue pads.
+       Disconnected pads on CPX produce slightly noisy 12bit ADC values.
+       Shuffling bits around a little to distribute that noise."""
+    a2 = analogio.AnalogIn(board.A2)
+    a3 = analogio.AnalogIn(board.A3)
+    a4 = analogio.AnalogIn(board.A4)
+    a5 = analogio.AnalogIn(board.A5)
+    random_value = ((a2.value >> 4) + (a3.value << 1) +
+                    (a4.value << 6) + (a5.value << 11))
+    for pin in (a2, a3, a4, a5):
+        pin.deinit()
+    random.seed(random_value)
+
+# Without os.urandom() the random library does not set a useful seed
+try:
+    os.urandom(4)
+except NotImplementedError:
+    seed_with_noise()
 
 # create a PWMOut object on pad A6
 pwma6 = pulseio.PWMOut(board.A6, duty_cycle=2 ** 15, frequency=50)
@@ -54,15 +89,24 @@ speaker_enable.direction = digitalio.Direction.OUTPUT
 speaker_on = True
 speaker_enable.value = speaker_on
 
-dac = audioio.AudioOut(board.SPEAKER)
+dac = AudioOut(board.SPEAKER)
+
+# Number of seconds
+SHORTEST_DELAY = 3.0
+LONGEST_DELAY = 7.0
+
+red = (40, 0, 0)
+black = (0, 0, 0)
 
 A4refhz = 440
 midpoint = 32768
 twopi = 2 * math.pi
 
-# A sawtooth function like math.sin(angle)
-# 0 returns 1.0, pi returns 0.0, 2*pi returns -1.0
+
 def sawtooth(angle):
+    """A sawtooth function like math.sin(angle).
+    Input of 0 returns 1.0, pi returns 0.0, 2*pi returns -1.0."""
+    
     return 1.0 - angle % twopi / twopi * 2
 
 # make a sawtooth wave between +/- each value in volumes
@@ -78,58 +122,51 @@ waveraw = array.array("H",
                                            + math.pi))
                       for idx in range(sample_len)])
 
-beep = audioio.RawSample(waveraw, sample_rate = sample_len * A4refhz)
+beep = RawSample(waveraw, sample_rate = sample_len * A4refhz)
 
-clicks = audioio.RawSample(array.array("H", [midpoint, 65535, 0] + [midpoint] * 93),
-                           sample_rate = 4 * 100)
-
-# play something to get things initialised
+# play something to get things inside audio libraries initialised
 dac.play(beep, loop=True)
-time.sleep(0.050)
+time.sleep(0.1)
 dac.stop()
+dac.play(beep)
 
-dac.play(clicks, loop=True)
-time.sleep(0.5)
-dac.stop()
 
 # brightness 1.0 saves memory by removing need for a second buffer
-# 10 is number of NeoPixels on CPX
+# 10 is number of NeoPixels on CPX/CPB
 numpixels = const(10)
 pixels = neopixel.NeoPixel(board.NEOPIXEL, numpixels, brightness=1.0)
 
-### CPX touchpad (A0 cannot be used)
+### CPX/CPB touchpad (A0 cannot be used)
 touchpad = touchio.TouchIn(board.A1)
 
-### a is left (usb at top)
-buttonleft = digitalio.DigitalInOut(board.BUTTON_A)
-buttonleft.switch_to_input(pull=digitalio.Pull.DOWN)
-buttonright = digitalio.DigitalInOut(board.BUTTON_B)
-buttonright.switch_to_input(pull=digitalio.Pull.DOWN)
+def wait_finger_off_and_random_delay():
+    while touchpad.value:
+        pass
+    duration = (SHORTEST_DELAY +
+                random.random() * (LONGEST_DELAY - SHORTEST_DELAY))
+    time.sleep(duration)
 
 ### servo on a CPX appears to be risky, possibly placing
 ### the audio amp at risk of over-heating
 ### https://forums.adafruit.com/viewtopic.php?f=58&t=157190
 tactile_enable = False
 
-run=1
+run = 1
 
+### TODO - convert to single print and add stats
 while True:
-    while touchpad.value:
-        pass
-    time.sleep(3.0 + random.random() * 4.0)
+    wait_finger_off_and_random_delay()
     gc.collect()
-    pixels[0] = (40, 0, 0)
+    pixels[0] = red
     start_t = time.monotonic()
     while not touchpad.value:
         pass
     react_t = time.monotonic()
     reaction_dur = react_t - start_t
     print("Trial ", run, ": visual reaction time is ", reaction_dur)
-    pixels[0] = (0, 0, 0)
-        
-    while touchpad.value:
-        pass
-    time.sleep(3.0 + random.random() * 4.0)
+    pixels[0] = black
+
+    wait_finger_off_and_random_delay()
     gc.collect()
     dac.play(beep, loop=True)
     start_t = time.monotonic()
@@ -139,11 +176,9 @@ while True:
     reaction_dur = react_t - start_t
     print("Trial ", run, ": audio reaction time is ", reaction_dur)
     dac.stop()        
-    
+
     if tactile_enable:
-        while touchpad.value:
-            pass
-        time.sleep(3.0 + random.random() * 4.0)
+        wait_finger_off_and_random_delay()
         gc.collect()
         servo.angle = 10
         start_t = time.monotonic()
@@ -155,10 +190,3 @@ while True:
         servo.angle = 0
 
     run += 1
-    
-    if buttonleft.value:
-        while buttonleft.value:
-            pass   ### wait for button up
-    if buttonright.value:
-        while buttonright.value:
-            pass   ### wait for button up
