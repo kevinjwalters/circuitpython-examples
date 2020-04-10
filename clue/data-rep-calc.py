533d5d148b624806c60f546f2a85c060c90500bd
Kevin J Walters <kjw.github@gmail.com>
2020-04-10 19:01:32 +0100

    Adding cursor support to ScreenFP, starting with a stable red underline. Hiding that cursor when exponent is changed but would be good to have exponent underlined too. Provisionally sticking three values on screen to test spacing - unfinished. Removing the accidental double operand1.cursor_right() to fix bug.

--- clue/data-rep-calc.py
+++ clue/data-rep-calc.py
@@ -1,4 +1,4 @@
-### data-rep-calc v0.9
+### data-rep-calc v0.11
 ### A calculator which also shows the floating point data representation
 
 ### Tested with an Adafruit CLUE (Alpha) and CircuitPython and 5.1.0
@@ -35,13 +35,18 @@ import board
 import displayio
 import terminalio
 
+### TODO negative number
+### TODO flip between op1 / op2
+### TODO show operator
+### TODO change operator
+### TODO calculate result using CP's FP
+
 ### TODO make this work for bluefruit + gizmo too
+### And PyGamer / any Arcade device which runs CircuitPython
 from adafruit_clue import clue
 from adafruit_display_text.label import Label
 
 debug = 1
-
-### TODO - implement a hold left press to change position of decimal point
 
 
 ### TODO is looking for board.DISPLAY a reasonable way to work out if we are on CPB+Gizmo
@@ -58,17 +63,22 @@ def d_print(level, *args, **kwargs):
 RAD_TO_DEG = 180.0 / math.pi
 
 
-
-
 digit = 5
 
 changed = False
 
 ### lilac, pale green and beige inspired bytearray
 ### https://www.h-schmidt.net/FloatConverter/IEEE754.html
-signbg = (211, 211, 232)
-exponentbg = (194, 222, 195)
-mantissabg = (222, 209, 197)
+### first set are actual values, second set tweaked for CLUE
+### signbg = (211, 211, 232)
+### exponentbg = (194, 222, 195)
+### mantissabg = (222, 209, 197)
+signbg_col = (211, 211, 232)
+exponentbg_col = (194, 232, 195)
+mantissabg_col = (232, 209, 187)
+
+cursor_col = (255, 0, 0)  ### Red cursor
+
 
 def get_tilt_angle():
     """ 0 degrees for flat on a desk, 90 for upright """
@@ -139,9 +149,6 @@ class DecimalFP():
         self._fp30bit = None
         self._recalc_fp()
 
-    def cursor_right(self):
-        self._cursor = (self._cursor + 1) % self._precision
-
     def __str__(self):
         """Returns the value in scientific notation."""
         return self.text_repr()
@@ -176,20 +183,23 @@ class DecimalFP():
         mantissa = tuple(([1] if implicit else []) + bits[9:32])
         return (sign, exponent, mantissa)
 
-    def cursor_x_pos(self):
-        """The x position of the cursor is very similar to cursor property but this value
-           takes into account decimal point placement."""
-        if self._cursor == 0:
-            return self._cursor
-        return self._cursor + 1  ### skip over decimal point
-        ### TODO do I want to return two values here?
-
     def _recalc_fp(self):
         """Recalculate the floating point version of the number using
            CircuitPython's float() with a string made from the authoritative data."""
         self._fp30bit = float(str(self._mantissa[0]) + "."
                               + "".join(map(str, self._mantissa[1:]))
                               + "e" + str(self._exponent))
+
+    def cursor_right(self):
+        self._cursor = (self._cursor + 1) % self._precision
+
+    @property
+    def cursor(self):
+        return self._cursor
+
+    @cursor.setter
+    def cursor(self, value):
+        self._cursor = value
 
     @property
     def cursor_digit(self):
@@ -201,14 +211,6 @@ class DecimalFP():
             raise ValueError("Not 0-9 int")
         self._mantissa[self._cursor] = value
         self._recalc_fp()
-
-    @property
-    def cursor(self):
-        return self._cursor
-
-    @cursor.setter
-    def cursor(self, value):
-        self._cursor = value
 
     @property
     def exponent(self):
@@ -223,6 +225,9 @@ class DecimalFP():
 class ScreenFP():
     zeros_and_ones = None  ### initialised in constructor
     z_a_o_palette = None  ### initialised in constructor
+    _cursors = {}
+
+    _COLORS = 7
 
     BIT_WIDTH = 7
     BIT_HEIGHT = 8
@@ -234,12 +239,42 @@ class ScreenFP():
     MANTISSA_0 = 4
     MANTISSA_1 = 5
 
+    _FONT = terminalio.FONT
+    _FONT_W = _FONT.get_bounding_box()[0]
+    _FONT_H = _FONT.get_bounding_box()[1]
+
+    @classmethod
+    def _make_cursor_line(cls, size):
+        """Make a simple line cursor in Bitmap form assuming it will be scaled
+           like the decimal text field is scaled."""
+        width = (cls._FONT_W - 1) * size
+        height = 2 * size
+        cursor_line = displayio.Bitmap(width, height, cls._COLORS)
+
+        for pix_idx in range(width * height):
+            cursor_line[pix_idx] = 6  ### TODO
+
+        return cursor_line
+
+    @classmethod
+    def _make_cursor(cls, cursor_type, size):
+        """Create a new cursor if required, otherwise just return an existing one."""
+        cursor = cls._cursors.get((cursor_type, size))
+        if cursor is None:
+            if cursor_type == "line":
+                cursor = cls._make_cursor_line(size)
+            else:
+                return ValueError("cursor_type must be line")
+            cls._cursors[(cursor_type, size)] = cursor
+
+        return cursor
+
     @classmethod
     def _make_zeros_and_ones(cls):
         """A bitmap with six cls.BIT_WIDTHs in three 0 and 1 pairs, the three pairs having
            background colours of lilac, pale green and beige."""
         zeros_and_ones = displayio.Bitmap(6 * cls.BIT_WIDTH,
-                                          cls.BIT_HEIGHT, 6)
+                                          cls.BIT_HEIGHT, cls._COLORS)
 
         ### Draw background colours but leave right column blank for gap
         col_idx = (3, 4, 5)
@@ -276,47 +311,50 @@ class ScreenFP():
 
     @classmethod
     def _make_z_a_o_palette(cls):
-        z_a_o_palette = displayio.Palette(6)
+        z_a_o_palette = displayio.Palette(cls._COLORS)
         z_a_o_palette.make_transparent(0)
-        z_a_o_palette[0] = 0x000000
         z_a_o_palette[1] = 0x000000
         z_a_o_palette[2] = 0xffffff
-        z_a_o_palette[3] = signbg
-        z_a_o_palette[4] = exponentbg
-        z_a_o_palette[5] = mantissabg
+        z_a_o_palette[3] = signbg_col
+        z_a_o_palette[4] = exponentbg_col
+        z_a_o_palette[5] = mantissabg_col
+        z_a_o_palette[6] = cursor_col
         return z_a_o_palette
 
-    def __init__(self, size="tiny"):
+    def __init__(self, *, x=20, y=6, size="tiny", bits=32, cursor_type="line"):
         ### Must use class here for LHS assignment
         if ScreenFP.zeros_and_ones is None:
             ScreenFP.zeros_and_ones = ScreenFP._make_zeros_and_ones()
         if ScreenFP.z_a_o_palette is None:
             ScreenFP.z_a_o_palette = ScreenFP._make_z_a_o_palette()
 
-        self._decimal = Label(font=terminalio.FONT,
-                              text="-" * 32, color=0xc0c0c0,
-                              scale=2)
-        # Set the location
-        self._decimal.x = 20
-        self._decimal.y = 20
-
         if size=="tiny":
-            cols = 32
+            cols = bits
             rows = 1
             scale = 1
         elif size=="small":
-            cols = 16
+            cols = bits // 2
             rows = 2
             scale = 2
         else:
             raise ValueError("size must be tiny or small")
 
+        self._x = x   ### TODO - ponder this as it's currently indented from the binary representation
+        self._y = y
+        
+        ### TODO magic number
+        self._decimal = Label(font=self._FONT,
+                              text="-" * 20, color=0xc0c0c0,
+                              scale=scale)
+        # Set the location
+        self._decimal.x = self._x
+        self._decimal.y = self._y + self._FONT_H * scale // 2
+
         self._binary_chunk_width = cols
         self._binary_chunks = []
+
+        y_start_pos = self._y + round(1.25 * self._FONT_H * scale)
         
-        ### _, font_h = self._font.get_bounding_box()
-        
-        y_start_pos = 50 // scale
         for row in range(rows):
             bin_digits = displayio.TileGrid(self.zeros_and_ones,
                                             pixel_shader=self.z_a_o_palette,
@@ -324,22 +362,41 @@ class ScreenFP():
                                             height=1,
                                             tile_width=self.BIT_WIDTH,
                                             tile_height=self.BIT_HEIGHT)
-            bin_digits.x = 10 // scale
-            bin_digits.y = y_start_pos
-            y_start_pos += self.BIT_HEIGHT + 4  ### magic value
+            bin_digits.x = self._x // 2 // scale
+            bin_digits.y = y_start_pos // scale
+            y_start_pos += self.BIT_HEIGHT * scale + 4  ### magic value
             self._binary_chunks.append(bin_digits)
+
+        self._cursor_spacing = self._FONT_W * scale
+        self._cursor_visible = True
+
+        ### Get a shared cursor, TileGrid allows custom positioning
+        ### to allow for x/y positioning
+        cursor = ScreenFP._make_cursor(cursor_type, 2)
+        cursor_tilegrid = displayio.TileGrid(cursor,
+                                             pixel_shader=self.z_a_o_palette)
+        cursor_tilegrid.x = self._decimal.x
+        cursor_tilegrid.y = self._decimal.y + self._FONT_H // 2 + 5
 
         ### The binary row(s) need to go in as a second Group to allow them
         ### to have their own scale factor
-        main_group = displayio.Group(max_size=2)
+        main_group = displayio.Group(max_size=3)
         bin_group = displayio.Group(max_size=len(self._binary_chunks), scale=scale)
         main_group.append(self._decimal)
         main_group.append(bin_group)
+        main_group.append(cursor_tilegrid)
 
         for binary_chunk in self._binary_chunks:
             bin_group.append(binary_chunk)
+
+        self._cursor_tilegrid = cursor_tilegrid
         self._group = main_group
-        self._set_binary([0] * 32)
+
+        self._set_binary([0] * bits)
+
+        ### An optional on-screen representation of a cursor
+        ### None or 0
+        self._cursor = 0 if cursor_type else None
 
     def _set_binary(self, bits):
         for src_bit_idx in range(32):
@@ -366,18 +423,59 @@ class ScreenFP():
     def displayio_group(self):
         return self._group
 
+    def _cursor_set_x_pos(self):
+        """."""
+        if self._cursor is None:
+            return
+
+        ### skip the decimal point
+        cursor_pos = self._cursor if self._cursor == 0 else self._cursor + 1
+        self._cursor_tilegrid.x = 20 + cursor_pos * self._cursor_spacing  ### TODO
+
+    @property
+    def cursor(self):
+        return self._cursor
+
+    @cursor.setter
+    def cursor(self, value):
+        self._cursor = value
+        self._cursor_set_x_pos()
+
+    @property
+    def cursor_visible(self):
+        return self._cursor_visible
+
+    @cursor_visible.setter
+    def cursor_visible(self, value):
+        """If the value has changed store it and then hide or show it by removing it
+           from the main displayio Group."""
+        if self._cursor_visible != value:
+            self._cursor_visible = value
+            if self._cursor_visible:
+                self._group.append(self._cursor_tilegrid)
+            else:
+                self._group.pop()
+
 
 operators = ("+", "-", "*", "/", "^")
 
 ### default is tiny which is a challenge to read for oldsters!
-operand1_gob = ScreenFP(size="small")
+operand1_gob = ScreenFP(y=00, size="small")
+operand2_gob = ScreenFP(y=85, size="small")
+result_gob = ScreenFP(y=170, size="small")
 
 operand1 = DecimalFP()
 operand2 = DecimalFP()
 operator = operators[0]
 result = DecimalFP()
 
-display.show(operand1_gob.displayio_group())
+
+screen_group = displayio.Group(max_size=3)
+screen_group.append(operand1_gob.displayio_group())
+screen_group.append(operand2_gob.displayio_group())
+screen_group.append(result_gob.displayio_group())
+
+display.show(screen_group)
 
 
 ### TODO draw a cursor with colour showing whether in edit mode
@@ -391,15 +489,15 @@ while True:
     si, ex, ma = operand1.binary_fp_comp()
     operand1_gob.update_value(str(operand1), si + ex + ma)
 
+    ### TODO - replace this
+    operand2_gob.update_value(str(operand1), si + ex + ma)
+    result_gob.update_value(str(operand1), si + ex + ma)
+
     if changed:
         changed = False
         time.sleep(0.25)
 
     if clue.button_a and not clue.button_b:
-        ### TODO - change this to measure the press time and if > 1s
-        ### go into exponent change mode on left/right tilt
-        operand1.cursor_right()
-        #digit_idx = ( digit_idx + 1 ) % len(digits)
         timeout = False
         start_ns = time.monotonic_ns()
         while clue.button_a:
@@ -411,6 +509,7 @@ while True:
             ### TODO visual indication exponent is being changed
             start_tr_angle, _ = get_tilt_angles()
             start_exponent = operand1.exponent
+            operand1_gob.cursor_visible = False
             while clue.button_a:
                 tr_angle, _ = get_tilt_angles()
                 offset = int((tr_angle - start_tr_angle) / 5.0)
@@ -422,8 +521,12 @@ while True:
                 ##text_area.text = str(operand1)
                 si, ex, ma = operand1.binary_fp_comp()
                 operand1_gob.update_value(str(operand1), si + ex + ma)
+            operand1_gob.cursor_visible = True
         else:
+            ### Move the cursor on the held by the data representation
+            ### then update the screen version
             operand1.cursor_right()
+            operand1_gob.cursor = operand1.cursor
 
     if clue.button_b:
         ## (gx, _, _) = clue.gyro
