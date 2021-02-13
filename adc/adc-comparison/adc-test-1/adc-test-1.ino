139e74907e6d189fe761a6cec8156c79c3368521
Kevin J Walters <kjw.github@gmail.com>
2021-02-13 00:22:17 +0000

    Adjustments to make it work on Arduino Uno as well as ESP32. Have to use dtostrf on Arduino Uno as print functions like snprintf do not support %f. Using custom type to deal with float on Uno and double on ESP32.

--- adc/adc-comparison/adc-test-1/adc-test-1.ino
+++ adc/adc-comparison/adc-test-1/adc-test-1.ino
@@ -2,6 +2,11 @@
 // Print values from ADC to compare between boards
 // Port of CircuitPython version intended for ESP boards
 
+#ifdef ESP32
+#define fptype double
+#else
+#define fptype float
+#endif
 
 int input_pin = 4;  // GP4 on ESP32
 unsigned int sample_sizes[] = {1, 8, 50};
@@ -12,23 +17,32 @@ int dummy_out_value = -1;
 char output_line[OUTPUT_LINE_LEN];  // not NUL terminated
 
 #define TIME_FN() (micros() - program_start_time)
-double time_mul = 1e-6;
+fptype time_mul = 1e-6;
 unsigned long program_start_time = 0L;
 unsigned long last_loop = 0L;
 unsigned long interval = 25L * 1000L; // 25ms
 
+#ifdef ESP32
 #define SAMPLE_BITS 12
+#else
+#define SAMPLE_BITS 10
+#endif
 double cp_mul = 65535.0 / ((1 << SAMPLE_BITS) - 1);
 
 
 void setup() {
+#ifndef ESP32
+    analogReference(EXTERNAL);  // Set to 3.3V
+#endif
     Serial.begin(115200);
     while (!Serial);  // wait for it to be ready
 
+#ifdef ESP32
   // ESP32 defaults to 12bit -11dB
   // Some reports that 11bit has better performance >3.1V
   analogSetWidth(SAMPLE_BITS);
   analogReadResolution(SAMPLE_BITS);
+#endif
   program_start_time = TIME_FN();
 
   delay(30 * 1000);  // 30 second pause before starting
@@ -51,21 +65,35 @@ void loop() {
     for (int i=0; i < sample_sizes[s_idx]; i++) {
       sample_sum += analogRead(input_pin);
     }
-    double in_value = (double)sample_sum / (double)sample_sizes[s_idx];
+    fptype in_value = (fptype)sample_sum / (fptype)sample_sizes[s_idx];
+#ifdef ESP32
     snprintf(output_line,
-             sizeof(output_line) / sizeof(output_line[0]),
+             sizeof(output_line),
              "%f,%d,%u,%f",
-             (double)in_start_t * time_mul,
+             (fptype)in_start_t * time_mul,
              dummy_out_value,
              sample_sizes[s_idx],
              in_value * cp_mul); // convert to 0-65535.0 range
-
+#else
+    // 1+5+1+6+1 = 14
+    static char str_float1[14], str_float2[14];
+    dtostrf((fptype)in_start_t * time_mul, 5, 6 ,str_float1);
+    dtostrf(in_value * cp_mul, 5, 6 ,str_float2);
+    snprintf(output_line,
+             sizeof(output_line),
+             "%s,%d,%u,%s",
+             str_float1,
+             dummy_out_value,
+             sample_sizes[s_idx],
+             str_float2);
+#endif
     // Pad white whitespace but leave room for CRLF
     for (int i = strlen(output_line); i < (OUTPUT_LINE_LEN - 2) ; i++) {
       output_line[i] = ' ';
     }
+    // This is not NUL terminated
     output_line[OUTPUT_LINE_LEN - 2 ] = '\r';  // CR
-    output_line[OUTPUT_LINE_LEN - 2 ] = '\n';  // LF
+    output_line[OUTPUT_LINE_LEN - 1 ] = '\n';  // LF
     Serial.write(output_line, OUTPUT_LINE_LEN);
   }
 }
