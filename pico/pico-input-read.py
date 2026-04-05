5df1f764009018b531db5f276b2ea30ee74737e2
Kevin J Walters <kjw.github@gmail.com>
2026-04-05 22:57:06 +0100

    Adding commands to read multiple (32) values

--- pico/pico-input-read.py
+++ pico/pico-input-read.py
@@ -1,4 +1,4 @@
-### pico-gpio-read v1.0
+### pico-input-read v1.1
 ### Respond to simple serial commands with digital or analogue gpio
 
 ### Tested on Pi Pico W vs Pi Pico 2 W both running CircuitPython 10.1.3
@@ -31,6 +31,7 @@
 
 ### Responses to commands sent over a serial/UART connection
 ### with digital or adc value from GP26
+### Multiple read commands addded for noise analysis
 
 ### Relevant Errata: RP2350-9 and RP2040-E11
 
@@ -52,29 +53,39 @@ serial = busio.UART(tx=SERIAL_TX_PIN, rx=SERIAL_RX_PIN,
                     baudrate=SERIAL_BAUDRATE, timeout=CMD_READ_TIMEOUT_S)
 
 READ_ANA_CMD = "A"
+READM_ANA_CMD = "B"
+
 READ_DIG_CMD = "D"
+READM_DIG_CMD = "E"
 
 gpio = None
 
 
 SAMPLE_COUNT = 32
-samples = [0] * SAMPLE_COUNT
-def get_sample_analogue(ana):
+sample_store = [0] * SAMPLE_COUNT
+def get_sample_analogue(ana, samples=None):
     for idx in range(SAMPLE_COUNT):
-        samples[idx] = ana.value
-    samples.sort()
+        sample_store[idx] = ana.value
+
+    ### Make a copy of samples before sorting if requested
+    if samples is not None:
+        samples[:] = sample_store
+
+    sample_store.sort()
     total = 0
     ### Discard bottom two and top two for IQR style arithmetic mean
     for idx in range(2, SAMPLE_COUNT - 2):
-        total += samples[idx]
+        total += sample_store[idx]
     return total / (SAMPLE_COUNT - 4)
+
 
 def get_digital(dig):
     return 1 if dig.value else 0
 
+
 time.sleep(5)
 
-
+original_samples = [0] * SAMPLE_COUNT
 while True:
     serial_request = serial.readline()
     try:
@@ -83,23 +94,31 @@ while True:
         cmds = []
 
     for cmd in cmds:
-        if cmd == READ_ANA_CMD:
+        value = None
+        if cmd in (READ_ANA_CMD, READM_DIG_CMD):
             ### Ensure input is correct type
             if not isinstance(gpio, analogio.AnalogIn):
                 if gpio is not None:
                     gpio.deinit()
                 gpio = analogio.AnalogIn(GPIO_PIN)
 
-            value = get_sample_analogue(gpio)
-            serial.write(f"{value}\n".encode("utf-8"))
-        elif cmd == READ_DIG_CMD:
+            if cmd == READ_ANA_CMD:
+                value = get_sample_analogue(gpio)
+            else:
+                _ = get_sample_analogue(gpio, original_samples)
+                value = ",".join(original_samples)
+        elif cmd in (READ_DIG_CMD, READM_DIG_CMD):
             ### Ensure input is correct type
             if not isinstance(gpio, digitalio.DigitalInOut):
                 if gpio is not None:
                     gpio.deinit()
                 gpio = digitalio.DigitalInOut(GPIO_PIN)
 
-            value = get_digital(gpio)
-            serial.write(f"{value}\n".encode("utf-8"))
+            if cmd == READ_DIG_CMD:
+                value = get_digital(gpio)
+            else:
+                value = ",".join([get_digital(gpio) for _ in range(SAMPLE_COUNT)])
         elif len(cmd) > 0:
-            serial.write("\n".encode("utf-8"))
+            value = ""
+        if value is not None:
+            serial.write(f"{value}\n".encode("utf-8"))
